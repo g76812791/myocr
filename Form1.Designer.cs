04f6b815bbcbade8804abe18c11c46c12b79f5e8
Bruce Wu <Bruce.Wu@sbdinc.com>
2021-08-18 15:00:59 +0800

    ini

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(140, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(904, 595);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.AutoScroll = true;
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1362, 595);
+            this.splitContainer1.SplitterDistance = 407;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 74);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(405, 338);
+            this.textBox1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1362, 595);
+            this.Controls.Add(this.splitContainer1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,171 @@
+﻿using IronOcr;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckTxt();
+        }
+
+        private void CheckTxt()
+        {
+            tuopan();
+            Thread.Sleep(1000);
+            var img = GetScreen();
+            pictureBox1.Image = img;
+            using (var input = new OcrInput())
+            {
+                input.AddImage(img);
+                IronTesseract Ocr = new IronTesseract();
+                Ocr.Language = OcrLanguage.ChineseSimplified;
+                Ocr.AddSecondaryLanguage(OcrLanguage.English);
+                var Result = Ocr.Read(input);
+                this.textBox1.Text = DateTime.Now.ToString("yyyy年MM月dd日hh时mm分ss秒") + "\r\n" + Result.Text;
+                if (Result.Text.Contains("生/成"))
+                {
+                    Console.WriteLine("TEST");
+                    StringBuilder strInfo = new StringBuilder();
+                    strInfo.Append(string.Format("Hello-----------------------------"));//邮件主体内容（自己拼接的）
+                    string fromEmail = "m76812791@outlook.com";//发件邮箱 自己邮箱的服务
+                    string emailPwd = "qq18741689356";//发件邮箱密码
+                    string toEmail = "1273955317@qq.com";//收件邮箱
+                    string emailTitle = "你好----------------------------";//邮件标题
+                    string emailContent = strInfo.ToString() + this.textBox1.Text;//邮件主体内容
+                    string SmtpHost = "smtp.office365.com";
+                    int SmtpPort = 587;
+                    if (SmtpMailSend(SmtpHost, SmtpPort, fromEmail, emailPwd, emailTitle, emailContent, true, toEmail))
+                    {
+                    }
+                    else
+                    {
+                    }
+                }
+            }
+        }
+        public Bitmap GetScreen()
+        {
+            Rectangle ScreenArea = Screen.GetWorkingArea(this);
+            Bitmap bmp = new Bitmap(ScreenArea.Width, ScreenArea.Height);
+
+            var leftX = (ScreenArea.Width - 300) / 2;
+            var leftY = (ScreenArea.Height - 300) / 2;
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(200, 150, 0, 0, new Size(500, 500));
+            }
+            return bmp;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CheckTxt();
+        }
+
+
+        public bool SmtpMailSend(string smtpHost, int smtpPort, string fromAddress, string mailPassword, string title, string body, bool isBodyHtml, params string[] recipient)
+        {
+            try
+            {
+
+                MailMessage myMail = new MailMessage();
+                myMail.From = new MailAddress(fromAddress);
+                foreach (string item in recipient)
+                {
+                    if (!string.IsNullOrEmpty(item))
+                        myMail.To.Add(new MailAddress(item));
+                }
+                myMail.Subject = title; //邮件标题
+                myMail.SubjectEncoding = Encoding.UTF8;//标题编码
+                myMail.Body = body;//邮件主体
+                myMail.BodyEncoding = Encoding.UTF8;
+                myMail.IsBodyHtml = isBodyHtml;//是否HTML
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = smtpHost;
+                smtp.Port = smtpPort; //Gmail的smtp端口
+                                      // mail.smtp.starttls.enable = true
+                smtp.EnableSsl = true;//Gmail要求SSL连接
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = new System.Net.NetworkCredential(fromAddress, mailPassword);
+                //smtp.EnableSsl = true; //Gmail要求SSL连接
+                //smtp.DeliveryMethod = SmtpDeliveryMethod.Network; //Gmail的发送方式是通过网络的方式，需要指定
+                try
+                {
+                    smtp.Send(myMail);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            //tuopan();
+        }
+
+        private void tuopan()
+        {
+            // if (this.WindowState == FormWindowState.Minimized)
+            this.Hide();
+            this.notifyIcon1.Visible = true;
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+            //this.notifyIcon1.Visible = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.F1:
+                  
+                    break;
+
+                case Keys.F2:
+                  
+                    break;
+            }
+
+            // 组合键
+            if (e.Modifiers == Keys.Alt && e.KeyCode == Keys.D0)
+            {
+                CheckTxt();
+            }
+            CheckTxt();
+
+            //if (e.KeyCode == Keys.S && e.Modifiers == Keys.Control)         //Ctrl+F1
+            //{
+            //    CheckTxt();
+            //}
+        }
+    }
+}
